16b77df9c05eae61645aaae36d8eb10f0b4df082
Jeff Wesner <jeff.wesner@usd.edu>
2025-04-21 15:11:34 -0500

    update custom functions

---
+++ code/custom_functions/old/get_baseline_preds.R
@@ -0,0 +1,67 @@
+sample_gamma = function(mean, sd, n = 1){
+  alpha = (mean / sd)^2
+  beta = mean / (sd^2)
+  rgamma(n, shape = alpha, rate = beta)
+}
+
+sample_gamma = Vectorize(sample_gamma)
+
+get_baseline_preds <- function(mod, num_iterations = 300) {
+  # Filter chemicals based on the model's chemical category
+  chem <- cas_names %>% 
+    filter(chemical_category == mod$data2$chemical_category)
+  
+  # Prepare water predictions
+  water_predictions <- modeled_water %>% 
+    filter(cas == chem$cas) %>% 
+    mutate(chemical = unique(chem$chemical_category),
+           HYBAS_ID = bit64::as.character.integer64(HYBAS_L12),
+           mean.conc.year.units = "log10_ug_l")
+  
+  # Pre-calculate values that are constant for all rows
+  mean_x <- mod$data2$mean_x$`scaled:center`
+  sd_x <- mod$data2$sd_x$`scaled:scale`
+  max_adult_conc <- mod$data2$max_y
+  
+  # Initialize list to store y_mg_kg results
+  y_mg_kg <- vector("list", num_iterations)
+  
+  # Calculate y_mg_kg for each iteration
+  for (i in 1:num_iterations) {
+    y_mg_kg[[i]] <- water_predictions %>% 
+      mutate(
+        water_ug_l_raw = 10^(mean.conc.year * mean.det.year),
+        x_s = (log(water_ug_l_raw) - mean_x) / sd_x,
+        y_mg_kg = max_adult_conc * exp(post_samples$b_Intercept[i] + post_samples$b_x_s[i] * x_s)
+      ) %>% 
+      select(HYBAS_L12, y_mg_kg)
+  }
+  
+  # Prepare kg_flux data
+  kg_flux <- flux_predictions_all %>% 
+    select(HYBAS_L12, mean, sd) %>% 
+    ungroup() %>% 
+    mutate(kg_flux = sample_gamma(n = 1, mean = mean, sd = sd))
+  
+  # Initialize list to store sum_mg results
+  sum_mg <- vector("list", length(y_mg_kg))
+  
+  # Calculate sum_mg for each y_mg_kg
+  for (i in 1:length(y_mg_kg)) {
+    sum_mg[[i]] <- y_mg_kg[[i]] %>% 
+      left_join(kg_flux %>% filter(mean > 0), by = "HYBAS_L12") %>% 
+      filter(!is.na(mean)) %>% 
+      mutate(chem_flux_mg_year = kg_flux * y_mg_kg) %>% 
+      ungroup() %>% 
+      summarise(sum = sum(chem_flux_mg_year, na.rm = TRUE), .draw = i)
+  }
+  
+  # Combine results and calculate global flux
+  result <- bind_rows(sum_mg) %>% 
+    mutate(global_flux_MT_peryr = sum / 1e9) %>% 
+    select(-sum) %>% 
+    mutate(chemical = chem$chemical_category,
+           cas = chem$cas)
+  
+  return(result)
+}
---
+++ code/custom_functions/old/get_baseline_values.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(brms)
+library(tidybayes)
+
+raw_contaminants = readRDS(file = "data/contaminants.rds") %>%
+  group_by(chemical_category) %>% 
+  mutate(max_y = max(adult_conc_ng_mg_dm,na.rm = T),
+         y_s = adult_conc_ng_mg_dm/max_y,
+         mean_water_conc_001 = 0.001*mean(water_conc_ug_l, na.rm = T),
+         log_water_conc_ugl_01 = log(water_conc_ug_l + mean_water_conc_001),
+         x_s = scale(log_water_conc_ugl_01))
+
+cont_corrections = raw_contaminants %>% 
+  group_by(chemical_category) %>% 
+  mutate(water_conc_ug_l = water_conc_ug_l + 0.001*mean(water_conc_ug_l, na.rm = T),
+         min_water_conc_ugl_05 = 0.5*min(water_conc_ug_l, na.rm = T),
+         mean_log_water = mean(log(water_conc_ug_l), na.rm = T),
+         sd_log_water = sd(log(water_conc_ug_l), na.rm = T),
+         min_x_s05 = (log(min_water_conc_ugl_05) - mean_log_water)/sd_log_water) %>% 
+  ungroup %>% 
+  distinct(chemical_category, max_y,  min_x_s05)
+
+baseline_list = list()
+
+for(i in 1:length(mod_list)){
+  baseline_list[[i]] = cont_corrections %>% 
+    filter(chemical_category == mod_list[[i]]$data2$chemical_category) %>% 
+    mutate(pub_name = "new",
+           y_max = mod_list[[i]]$data2$max_y,
+           x_s = min_x_s05) %>% 
+    add_epred_draws(mod_list[[i]], allow_new_levels = T) %>% 
+    mutate(.epred = .epred*y_max)
+  
+}
+
+baseline_values = bind_rows(baseline_list) %>% 
+  filter(chemical_category %in% c("Se", "Cu", "Zn")) 
+  group_by(chemical_category) %>% 
+  median_qi(.epred) %>% 
+  rename(tissue_ng_mg = .epred) %>% 
+  mutate(tissue_ng_mg = round(tissue_ng_mg, 1),
+         .lower = round(.lower, 1),
+         .upper = round(.upper, 1)) %>% 
+  pivot_longer(cols = c(tissue_ng_mg, .lower, .upper)) %>% 
+  mutate(value = case_when(chemical_category %in% c("Se", "Cu", "Zn") ~ value, TRUE ~ 0)) %>% 
+  pivot_wider(names_from = name, values_from = value)
